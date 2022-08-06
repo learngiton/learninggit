07e33e3730299a82f7742a1d9354b1b9fa1b9b0e
learnongit <neverloosejay@gmail.com>
2022-08-06 19:14:36 +0000

    thridcommit

---
+++ three.sh
@@ -0,0 +1,22 @@
+hi
+ajay
+hello git how r u 
+do more practice on git ...
+git init
+vi ajay.sh
+ajay.sh is in working dic ...
+to move from working area to staging area 
+git add file name
+............now file is in staging area
+to move from staging area to local repo we have to commit the cpde
+git commit -m "commit message" filename
+
+working/untracked area----staging / tracked area ------ local repo=======remote repo
+have tio made connection betwween remote repo to local repo
+
+git remote add alias "HTTPS..URL"
+....
+git push alias master.............
+
+
+working/untracked area----staging / tracked area ------ local repo=======remote repo ----done
